8e7db4d670b4f39988235580e177cd0f8091871a
mhshifat <mehedihassanshifat01@gmail.com>
2021-01-26 21:07:21 +0600

    📦 Ready to go live

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = process.env.API_URI || "http://localhost:5000";
+export const API_BASE_URL =
+  process.env.REACT_APP_API_URI || "http://localhost:5000";
 
 export const API_ENDPOINTS = {
   AUTH: {
